0ff6502b831c21d863543bd9883ed1e092071e06
przemjan <przemjanisz2@gmail.com>
2022-09-14 08:05:57 +0200

    commit nr 4

--- src/podstawy/Zmienne.java
+++ src/podstawy/Zmienne.java
@@ -1,3 +1,5 @@
+package podstawy;
+
 public class Zmienne {
     public static void main(String[] args) {
 
---
+++ src/stringPorownanie/CharIsNumber.java
@@ -0,0 +1,16 @@
+package stringPorownanie;
+
+public class CharIsNumber {
+    public static void main(String[] args) {
+        char litera1 = 'ą';
+        char litera2 = 'b';
+
+        System.out.println(litera1+litera2);
+
+        int number = litera1;
+
+        System.out.println("Wartość litery a: "+ number);
+        //  1,3,6,7,9
+
+    }
+}
